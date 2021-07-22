25e875fe36889c744dd8d3ba213eefebf25cdd0e
S <sonyfebrian362@gmail.com>
2021-07-22 18:25:49 +0700

    first commit

---
+++ src/server.js
@@ -0,0 +1,46 @@
+require('dotenv').config();
+
+const Hapi = require('@hapi/hapi');
+const songs = require('./api/songs');
+const SongsService = require('./services/postgres/SongsService');
+const SongsValidator = require('./validator/songs');
+
+const users = require('./api/users');
+const UsersService = require('./services/postgres/UsersService');
+const UsersValidator = require('./validator/users');
+
+const init = async () => {
+  const songsService = new SongsService();
+  const usersService = new UsersService();
+  const server = Hapi.server({
+    port: process.env.PORT,
+    host: process.env.HOST,
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  await server.register([
+    {
+      plugin: songs,
+      options: {
+        service: songsService,
+        validator: SongsValidator,
+      },
+    },
+    {
+      plugin: users,
+      options: {
+        service: usersService,
+        validator: UsersValidator,
+      },
+    },
+  ]);
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
